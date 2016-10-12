ec8af1bf5eed045b960c8887eb64569df736374e
abe312 <abe312@users.noreply.github.com>
2016-10-13 00:08:41 +0530

    Update colors.js
    1) removed sanity checks.
    2) added automated id (by default it is in body element)

--- colors.js
+++ colors.js
@@ -1,26 +1,15 @@
-
-
 $( document ).ready(function() {
-	queue = new Array();
+	$('body').attr('id', 'colors');
 	function hiliter(word, element) {
-    var rgxp = new RegExp("\\b"+ word + "\\b" , 'gi');
-    console.log("key ", word);
-    console.log("regexp ", rgxp);
-    console.log(element.innerHTML);
+    var rgxp = new RegExp("\\b" + word + "\\b" , 'gi');
     var repl = '<span style="color:' + colorkeyval[word] +  ';">' + word + '</span>';
     element.innerHTML = element.innerHTML.replace(rgxp, repl);
-    console.log("repl ", repl);
 };
 	for(var key in colorkeyval){
 	    hiliter(key, document.getElementById('colors'));
 	};
 
 });
-
-
-
-
-
 var colorkeyval={
     black: '#000000',
     orange: '#ffa500',
